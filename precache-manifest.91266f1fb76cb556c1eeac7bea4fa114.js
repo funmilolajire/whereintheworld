8a394ba559e38e60dea72d1d5a9f0d882d043ad4
Funmilola O <funmilolajire@gmail.com>
2020-09-02 23:58:01 +0100

    Updates

---
+++ precache-manifest.91266f1fb76cb556c1eeac7bea4fa114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22697b818e870cd3df353201f5102606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4020558ab7dbbcf1813",
+    "url": "/static/css/main.1ec9c0f8.chunk.css"
+  },
+  {
+    "revision": "106d244106c62a56fce9",
+    "url": "/static/js/2.05246087.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05246087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4020558ab7dbbcf1813",
+    "url": "/static/js/main.b82608dc.chunk.js"
+  },
+  {
+    "revision": "faa9b1cf2131ba26c1cd",
+    "url": "/static/js/runtime-main.a5d352d5.js"
+  }
+]);
